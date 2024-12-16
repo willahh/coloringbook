5e0d8327bf73622cbadac895b578b489a39b42ad
William Ravel <wravel@gmail.com>
2024-12-16 16:19:37 +0100

    Update vite config

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  /**
+   * Build directory, use of dir "/docs" to match default github Page deploy directory
+   */
+  build: { outDir : "./../docs"},
+
+  /**
+   * Base dir for deploy : use of github repository name.
+   * Used to generate relative path e.g. <script src="/coloringbook/assets/index.js">
+   * https://vitejs.fr/guide/static-deploy.html
+   */
+  base: '/coloringbook/'
+});
