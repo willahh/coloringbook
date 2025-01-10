913df5faf84005f6fc763a79660403e8f6899066
William Ravel <wravel@gmail.com>
2025-01-10 21:36:04 +0100

    Add custom fontSize configurations

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -81,7 +81,22 @@ export default {
       },
       fontFamily: {
         mono: ['roboto-mono-regular', 'Courier New', 'Courier', 'monospace'],
-        sans: ['roboto', 'Arial', 'Sans-serif']
+        sans: ['roboto', 'Arial', 'Sans-serif'],
+      },
+      fontSize: {
+        xs: ['0.65rem', { lineHeight: '1rem' }],
+        sm: ['0.75rem', { lineHeight: '1.25rem' }],
+        base: ['0.875rem', { lineHeight: '1.5rem' }],
+        lg: ['1rem', { lineHeight: '1.75rem' }],
+        xl: ['1.125rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.25rem', { lineHeight: '2rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2.25rem' }],
+        '4xl': ['1.875rem', { lineHeight: '2.5rem' }],
+        '5xl': ['2.25rem', { lineHeight: '1' }],
+        '6xl': ['3rem', { lineHeight: '1' }],
+        '7xl': ['4rem', { lineHeight: '1' }],
+        '8xl': ['4.5rem', { lineHeight: '1' }],
+        '9xl': ['6rem', { lineHeight: '1' }],
       },
     },
   },
