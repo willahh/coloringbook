80c48ff3389427fbd680b259b2bf569600ca3fda
William Ravel <wravel@gmail.com>
2025-01-14 11:22:31 +0100

    WIP database prod settings

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,18 +1,42 @@
+import * as dotenv from 'dotenv';
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { User } from './users/entities/user.entity';
 import { Book } from './books/entities/book.entity';
 
-export const AppDataSource = new DataSource({
+// Charge les variables d'environnement depuis le fichier .env
+dotenv.config();
+
+let host = '';
+if (process.env.DB_INSTANCE_NAME) {
+  // prod
+  host = `/cloudsql/${process.env.DB_INSTANCE_NAME}/`;
+} else {
+  host = process.env.DATABASE_HOST;
+}
+const port = Number(process.env.DATABASE_PORT);
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
+const database = process.env.DATABASE_NAME;
+
+console.log('-----');
+console.log('Start backend with env variables :');
+console.log('host', host);
+console.log('port', port);
+console.log('user', user);
+console.log('database', database);
+console.log('-----');
+
+export const options: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'admin',
-  password: 'admin',
-  database: 'postgres',
-  synchronize: true,
-  logging: false,
+  host: host,
+  port: port,
+  username: user,
+  password: password,
+  database: database,
   entities: [User, Book],
-  migrations: [],
-  subscribers: [],
-});
+  // synchronize: true,
+  logging: true,
+};
+
+export const AppDataSource = new DataSource(options);
