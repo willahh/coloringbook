58cc555390b5e4709be41b42a065903bf50e0a33
William Ravel <wravel@gmail.com>
2025-01-21 10:03:16 +0100

    Add upload management to the supabase storage service

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -20,6 +20,8 @@ import { UpdateBookDto } from './dto/update-book.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { SupabaseService } from 'src/supabase.service';
 import { createReadStream } from 'fs';
+import { unlink } from 'fs/promises';
+import * as sharp from 'sharp';
 
 @Controller('books')
 export class BooksController {
@@ -32,38 +34,59 @@ export class BooksController {
   @UseInterceptors(FileInterceptor('coverImage', multerOptions))
   async create(
     @Body() createBookDto: CreateBookDto,
-    @UploadedFile() coverImage?: Express.Multer.File, // Rendre le paramètre optionnel
-  ) {
-    if (coverImage) {
+    @UploadedFile(
       new ParseFilePipe({
         validators: [
           new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5 MB
           new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
         ],
-      }).transform(coverImage);
+      }),
+    )
+    coverImage?: Express.Multer.File, // Rendre le paramètre optionnel
+  ) {
+    let supabaseFilePath = '';
+    const prefix = 'cover_';
+    const newFileName = `${prefix}${coverImage.filename}`;
+
+    if (coverImage) {
+      // Resize the image
+      const resizedImagePath = join(
+        __dirname,
+        '..',
+        '..',
+        'uploads',
+        newFileName,
+      );
+      await sharp(coverImage.path)
+        .jpeg({ quality: 80 }) // Convert to JPEG with 80% quality
+        .resize(400, 400) // Resize to 400x400 pixels
+        .toFile(resizedImagePath);
+
+      // Upload file to Supabase storage
+      const supabase = this.supabaseService.getClient();
+      const bucketName = 'uploads';
+
+      // Read file from local disk
+      const fileStream = createReadStream(resizedImagePath);
+
+      const { data, error } = await supabase.storage
+        .from(bucketName)
+        .upload(newFileName, fileStream, {
+          contentType: coverImage.mimetype,
+          duplex: 'half',
+        });
+
+      // Remove the files from local disk after upload
+      await unlink(coverImage.path);
+      await unlink(resizedImagePath);
+
+      if (error) {
+        throw new Error(`Error when uploading file: ${error.message}`);
+      }
+
+      // Public file path
+      supabaseFilePath = data.path;
     }
-
-    // Upload file to Supabase storage
-    const supabase = this.supabaseService.getClient();
-    const bucketName = 'uploads';
-
-    // Read file from local disk
-    const filePath = join(__dirname, '..', '..', coverImage.path);
-    const fileStream = createReadStream(filePath);
-
-    const { data, error } = await supabase.storage
-      .from(bucketName)
-      .upload(coverImage.originalname, fileStream, {
-        contentType: coverImage.mimetype,
-        duplex: 'half',
-      });
-
-    if (error) {
-      throw new Error(`Error when uploading file: ${error.message}`);
-    }
-
-    // Public file path
-    const supabaseFilePath = data.path;
 
     return this.booksService.create(createBookDto, supabaseFilePath);
   }
