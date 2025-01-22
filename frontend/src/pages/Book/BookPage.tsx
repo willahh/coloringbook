87132d7aa1fafdda2b8cdbf84e39992cc3c03ff4
William Ravel <wravel@gmail.com>
2025-01-22 16:13:29 +0100

    Refactor frontend components

--- frontend/src/pages/Book/BookPage.tsx
+++ frontend/src/pages/Book/BookPage.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import Layout from '../layout';
 
-import { SpreadToolbar } from './components/mainPanel/SpreadToolbar';
-import { SideToolbar } from './components/SidePanel/SideToolbar';
-import { ColorPanel } from './components/SidePanel/ColorPanel';
-import { SidePanel } from './components/SidePanel/SidePanel';
-import { GraphicsPanel } from './components/SidePanel/GraphicsPanel';
-import { TemplatePanel } from './components/SidePanel/TemplatePanel';
-import { PagesPanel } from './components/SidePanel/PagesPanel';
-import { MainPage } from './components/mainPanel/MainPage';
-import { MainSpread } from './components/mainPanel/MainSpread';
-import { VerticalSeparator } from './components/SidePanel/VerticalSeparator';
-
-const MainPanel: React.FC<{
-  className?: string;
-  children?: React.ReactNode;
-}> = ({ className, children }) => {
-  return <main className={`${className || ''}`}>{children}</main>;
-};
+import { SpreadToolbar } from './mainPanel/SpreadToolbar';
+import { SideToolbar } from './SidePanel/SideToolbar';
+import { ColorPanel } from './SidePanel/ColorPanel';
+import { SidePanel } from './SidePanel/SidePanel';
+import { GraphicsPanel } from './SidePanel/GraphicsPanel';
+import { TemplatePanel } from './SidePanel/TemplatePanel';
+import { PagesPanel } from './SidePanel/PagesPanel';
+import { MainPage } from './mainPanel/MainPage';
+import { MainSpread } from './mainPanel/MainSpread';
+import { VerticalSeparator } from './SidePanel/VerticalSeparator';
 
 const BookPage: React.FC = () => {
   // const { id } = useParams<{ id: string }>();
@@ -26,7 +19,7 @@ const BookPage: React.FC = () => {
     <Layout className={`w-full flex`} showHeader={true}>
       <SidePanel
         className="flex flex-row bg-primary-900 w-72 shadow-black z-10
-      shadow-[3px_0_10px_-4px_rgba(0,0,0,0.3)]"
+      shadow-[3px_0_10px_-4px_rgb aa(0,0,0,0.3)]"
       >
         <div className="flex-1 border-r border-primary-950">
           <PagesPanel className="h-96" />
@@ -39,7 +32,7 @@ const BookPage: React.FC = () => {
         </div>
         <SideToolbar />
       </SidePanel>
-      <MainPanel className="flex flex-1 bg-slate-900 flex-col">
+      <main className="flex flex-1 bg-slate-900 flex-col">
         <div
           data-name="spread-container"
           className="flex-1 flex justify-center items-center p-10 snap-x overflow-x-auto"
@@ -50,7 +43,7 @@ const BookPage: React.FC = () => {
           </MainSpread>
         </div>
         <SpreadToolbar></SpreadToolbar>
-      </MainPanel>
+      </main>
     </Layout>
   );
 };
