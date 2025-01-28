b63fdd34ee3a0b874e7e802115c8d700d2e3decf
William Ravel <wravel@gmail.com>
2025-01-28 15:17:21 +0100

    Add appearance menu

--- frontend/src/pages/Book/SpreadViewerCanvas/ui/ToolbarButton.tsx
+++ frontend/src/pages/Book/SpreadViewerCanvas/ui/ToolbarButton.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 // import { Tooltip } from '@radix-ui/themes';
 import { Tooltip } from '@components/Tooltip';
 
+export const ToolbarButtonClassName = (className?: string) => {
+  return `rounded-lg w-12 h-12 p-1 flex items-center justify-center transition-all duration-300
+  text-white group border border-transparent
+  hover:bg-primary-900  hover:border-primary-500 hover:scale-110
+  focus-visible:outline focus-visible:outline-2 focus-visible:scale-110 focus-visible:outline-offset-2 focus-visible:outline-primary-600
+  ${className || ''}`;
+};
+
 export const ToolbarButton: React.FC<{
   className?: string;
   children?: React.ReactNode;
@@ -12,11 +20,7 @@ export const ToolbarButton: React.FC<{
     <button
       type="button"
       onClick={onClick}
-      className={`rounded-lg w-12 h-12 p-1 flex items-center justify-center transition-all
-text-white group border border-transparent
-hover:bg-primary-900  hover:border-primary-500
-focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600
-${className}`}
+      className={ToolbarButtonClassName(className)}
     >
       {children}
     </button>
