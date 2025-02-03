c18db6ba3a767242f7fa6db31ae6f3af1310d8c7
William Ravel <wravel@gmail.com>
2025-02-03 21:37:22 +0100

    Refactor Button component to support variant styles and improve props handling

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,58 +1,49 @@
 import React from 'react';
 
-interface ButtonProps {
-  /**
-   * Le texte du bouton ou le contenu du bouton
-   */
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-
-  /**
-   * La fonction à éxecuter lorsque le bouton est cliqué.
-   */
   onClick?: () => void;
-
-  /**
-   * Type du bouton, par défaut 'button'.
-   */
-  type?: 'button' | 'submit' | 'reset';
-
-  /**
-   * Si le bouton est en focus.
-   */
-  autoFocus?: boolean;
-
-  /**
-   * La classe CSS pour des styles supplémentaires.
-   */
   className?: string;
-
-  /**
-   * Si le bouton est désactivé.
-   */
-  disabled?: boolean;
-
-  /**
-   * Référence pour le bouton.
-   */
-  ref?: React.Ref<HTMLButtonElement>;
+  variant?: 'primary' | 'secondary';
 }
 
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
-  type,
+  type = 'button',
   autoFocus,
-  className,
+  className = '',
   disabled,
-  ref,
+  variant = 'primary',
+  ...props
 }) => {
-  const baseStyle = `py-3 px-6 rounded-md flex items-center space-x-4 select-none
+  // Base styles that are common to all variants
+  const commonStyles = `
+    py-3 px-6 rounded-md flex items-center space-x-4 select-none
     transition-all duration-300 ease-in-out 
-    bg-primary-100 dark:bg-primary-900 text-primary-900 dark:text-primary-100 border border-primary-200 dark:border-primary-800
-    dark:hover:bg-primary-700 hover:bg-primary-300 dark:hover:text-white
-    focus:outline-none focus:ring-2 focus:ring-primary-700 dark:focus:ring-primary-300 active:bg-primary-300 dark:active:bg-primary-700 focus:text-black dark:focus:text-white
-    disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-primary-800`;
-  const combinedStyle = `${baseStyle} ${className}`;
+    focus:outline-none focus:ring-2 focus:text-black dark:focus:text-white
+    disabled:opacity-50 disabled:cursor-not-allowed
+  `;
+
+  // Variant-specific styles
+  const variantStyles =
+    {
+      primary: `
+      bg-primary-100 dark:bg-primary-900 text-primary-900 dark:text-primary-100 border border-primary-200 dark:border-primary-800
+      hover:bg-primary-300 dark:hover:bg-primary-700 dark:hover:text-white
+      focus:ring-primary-700 dark:focus:ring-primary-300
+      active:bg-primary-300 dark:active:bg-primary-700 disabled:bg-primary-800
+    `,
+      secondary: `
+      bg-secondary-100 dark:bg-secondary-500 text-secondary-900 dark:text-secondary-100 border border-secondary-200 dark:border-secondary-800
+      hover:bg-secondary-300 dark:hover:bg-secondary-700 dark:hover:text-white
+      focus:ring-secondary-700 dark:focus:ring-secondary-300
+      active:bg-secondary-300 dark:active:bg-secondary-700 disabled:bg-secondary-800
+    `,
+    }[variant] || ''; // Fallback to empty string if variant doesn't exist
+
+  // Construct the combined style
+  const combinedStyle = `${commonStyles} ${variantStyles} ${className}`.trim();
 
   return (
     <button
@@ -61,7 +52,7 @@ const Button: React.FC<ButtonProps> = ({
       className={combinedStyle}
       disabled={disabled}
       {...(autoFocus ? { autoFocus: true } : {})}
-      ref={ref}
+      {...props}
     >
       {children}
     </button>
