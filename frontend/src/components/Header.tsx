4a53187e19455356698dcd98d1eed3e9e5cbe7a2
William Ravel <wravel@gmail.com>
2025-02-03 21:37:31 +0100

    Add theme-based logo rendering in Header component and adjust height

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,25 @@
 import Logo from '@assets/coloring-book-logo-wide.svg?react';
+import LogoLight from '@assets/coloring-book-logo-wide-light.svg?react';
+import { useTheme } from '@/contexts/ThemeContext';
+
 interface HeaderProps {
   className?: string;
   children?: React.ReactNode;
 }
 
 const Header: React.FC<HeaderProps> = ({ className, children }) => {
+  const { appearance } = useTheme();
   return (
     <header
-      className={`pr-6 h-20 flex items-center justify-end border-t border-primary-100 dark:border-primary-900 
+      className={`pr-6 h-10 xl:h-20 flex items-center justify-end border-t border-primary-100 dark:border-primary-900 
         ${className} bg-primary-50 dark:bg-primary-950 z-20`}
     >
       <div className="flex-1 pl-6">{children}</div>
-      <Logo className="w-36" />
+      {appearance === 'dark' ? (
+        <Logo className="w-24 xl:w-36" />
+      ) : (
+        <LogoLight className="w-24 xl:w-36" />
+      )}
     </header>
   );
 };
