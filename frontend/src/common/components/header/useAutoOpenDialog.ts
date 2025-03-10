1462420673b376e3c56cb3d6c3f60706b792bf93
William Ravel <wravel@gmail.com>
2025-03-10 12:09:32 +0100

    Refactor useAutoOpenDialog hook to simplify dialog opening logic and improve timer management

--- frontend/src/common/components/header/useAutoOpenDialog.ts
+++ frontend/src/common/components/header/useAutoOpenDialog.ts
@@ -1,7 +1,6 @@
 /**
  * This hook manages the automatic opening of a dialog based on the user's visit history.
- * It uses local storage to track the last time the dialog was shown and sets timers to open the dialog
- * on the first visit and subsequent visits after a specified delay.
+ * It ensures the dialog opens only once using local storage to track the last display.
  */
 
 import { useState, useEffect, useRef } from 'react';
@@ -9,33 +8,29 @@ import useLocalStorage from '@/common/hooks/useLocalStorage';
 
 // const SUBSEQUENT_VISIT_DE = 1 * 60 * 60 * 1000; // 1 heure
 // const SUBSEQUENT_VISIT_DE = 1 * 1 * 60 * 1000; // 1 minute
-const FIRST_VISIT_DELAY_____ = 1 * 1 * 17 * 1000; // 17 secondes
-const SUBSEQUENT_VISIT_DELAY = 1 * 60 * 60 * 1000; // 1 heure
+const FIRST_VISIT_DELAY = 1 * 1 * 17 * 1000; // 17 secondes
+// const SUBSEQUENT_VISIT_DELAY = 1 * 60 * 60 * 1000; // 1 heure
 
 export const useAutoOpenDialog = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [lastSeen, setLastSeen] = useLocalStorage('about-ts', 0);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const timeoutBRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
+    // Si la boîte n'a jamais été affichée (lastSeen = 0) ou si elle a été affichée 
+    // mais qu'on veut la réafficher une seule fois
     if (!lastSeen) {
-      // La fenêtre n'a jamais été affichée
       timeoutRef.current = setTimeout(() => {
         setIsOpen(true);
-        setLastSeen(Date.now());
-      }, FIRST_VISIT_DELAY_____);
+        setLastSeen(Date.now()); // Marque l'affichage comme terminé
+      }, FIRST_VISIT_DELAY); // Remplacez par votre constante si nécessaire
     }
 
-    timeoutBRef.current = setTimeout(() => {
-      setLastSeen(Date.now());
-      setIsOpen(true);
-    }, SUBSEQUENT_VISIT_DELAY);
+    // Nettoyage des timers au démontage
     return () => {
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
-      if (timeoutBRef.current) clearTimeout(timeoutBRef.current);
     };
-  }, []);
+  }, [lastSeen]); // Dépendance sur lastSeen pour éviter les recréations inutiles
 
   return { isOpen, setIsOpen };
 };
